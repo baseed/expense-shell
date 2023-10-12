2d93171bdc8203972854b375228f7a698efe3b39
baseed <baseed08@gmail.com>
2023-10-12 08:39:48 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -5,7 +5,9 @@ cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
 mkdir /app
+
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+
 cd /app
 unzip /tmp/backend.zip
 npm install
