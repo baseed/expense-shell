5f0eb7db064c212a177007313655b72bbea90b9a
baseed <baseed08@gmail.com>
2023-10-12 08:19:53 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -15,7 +15,7 @@ systemctl enable backend
 systemctl start backend
 
 dnf install mysql -y
-mysql -h 172.31.95.105  -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.95.105 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
 
 
