e3b358dacd9b45b8554ed564e1efe57ca4e918ef
baseed <baseed08@gmail.com>
2023-10-13 19:58:44 +0530

    expense script

--- frontend.sh
+++ frontend.sh
@@ -4,18 +4,22 @@ component=frontend
 
 echo installing nginx
 dnf install nginx -y &>>$log_file
-
+echo$?
 echo placing Expense config file
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo$?
 
 echo removing old nginx content
 rm -rf /usr/share/nginx/html/* &>>$log_file
+echo$?
 
 cd /usr/share/nginx/html
 
 download_and_extract
+echo$?
 
 echo start the nginx service
 systemctl enable nginx &>>$log_file
 
 systemctl restart nginx &>>$log_file
+echo$?
