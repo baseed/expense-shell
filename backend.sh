5595dbe4f53afec8735f700e6e661fab88d1b1be
baseed <baseed08@gmail.com>
2023-10-13 19:17:56 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -1,15 +1,16 @@
+
 source commen.sh
 component=backend
 
 echo install nodejs
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
-dnf install nodejs -y >>$log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+dnf install nodejs -y &>>$log_file
 
 echo copy Backend Service File
-cp backend.service /etc/systemd/system/backend.service >>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 echo add Application user
-useradd expense >>$log_file
+useradd expense &>>$log_file
 
 echo cleanup content
 rm -rf /app
@@ -22,18 +23,18 @@ download_and_extract
 
 
 echo download dependency=e
-npm install >>$log_file
+npm install &>>$log_file
 
 echo restart servoce
-systemctl daemon-reload >>$log_file
-systemctl enable backend >>$log_file
-systemctl start backend >>$log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 
 echo download mysql
-dnf install mysql -y >>$log_file
+dnf install mysql -y &>>$log_file
 
 echo load schema
-mysql -h 172.31.95.105 -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+mysql -h 172.31.95.105 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 
 
 
--- frontend.sh
+++ frontend.sh
@@ -3,19 +3,19 @@ source commen.sh
 component=frontend
 
 echo installing nginx
-dnf install nginx -y >>$log_file
+dnf install nginx -y &>>$log_file
 
 echo placing Expense config file
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 echo removing old nginx content
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 cd /usr/share/nginx/html
 
 download_and_extract
 
 echo start the nginx service
-systemctl enable nginx >>$log_file
+systemctl enable nginx &>>$log_file
 
-systemctl restart nginx >>$log_file
+systemctl restart nginx &>>$log_file
