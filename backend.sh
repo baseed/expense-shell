a8617fc8fd55fd1e134c4e98bd2aec6a27250aa1
baseed <baseed08@gmail.com>
2023-10-13 20:04:13 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -5,36 +5,37 @@ component=backend
 echo install nodejs
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 dnf install nodejs -y &>>$log_file
+echo $?
 
 echo copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-
+echo $?
 echo add Application user
 useradd expense &>>$log_file
-
+echo $?
 echo cleanup content
 rm -rf /app
-
+echo $?
 mkdir /app
 cd /app
 echo download app content
 
 download_and_extract
-
+echo $?
 
 echo download dependency=e
 npm install &>>$log_file
-
+echo $?
 echo restart servoce
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
-
+echo $?
 echo download mysql
 dnf install mysql -y &>>$log_file
-
+echo $?
 echo load schema
 mysql -h 172.31.95.105 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-
+echo $?
 
 
--- mysql.sh
+++ mysql.sh
@@ -1,16 +1,21 @@
 source commen.sh
 echo disable mysql
 dnf module disable mysql -y &>>$log_file
+echo $?
 
 echo copy mysql repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+echo $?
 
 echo download mysql server
 dnf install mysql-community-server -y &>>$log_file
+echo $?
 
 echo start the service
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
+echo $?
 
 echo setup root password
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+echo $?
