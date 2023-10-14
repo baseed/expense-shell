5d4d4bb8bcbd3efbd2363aca8e2316f6c01b8bb1
baseed <baseed08@gmail.com>
2023-10-14 07:59:00 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -6,34 +6,34 @@ echo install nodejs
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 dnf install nodejs -y &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
 
 echo copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
 
 echo add Application user
 useradd expense &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
 
 echo cleanup content
 rm -rf /app
 
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
 
 mkdir /app
@@ -41,18 +41,21 @@ cd /app
 echo download app content
 
 download_and_extract
+
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
 
-echo download dependency=e
+echo download dependence
+
 npm install &>>$log_file
+
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
 
 echo restart servoce
@@ -60,25 +63,23 @@ systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
 
 echo download mysql
 dnf install mysql -y &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
-
 echo load schema
 mysql -h 172.31.95.105 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCSEES
+  echo -e "\e[32mSUCSEES\e[0m"
   else
-    echo FAIED
+    echo "\e[31mFAIED\e[0m"
     fi
-
 
