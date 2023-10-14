9712330d0d50d8c11c1c5a2369e73b6de8a62981
baseed <baseed08@gmail.com>
2023-10-14 09:01:38 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -5,40 +5,21 @@ component=backend
 echo install nodejs
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 dnf install nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAIED\e[0m"
-    exit 1
-    fi
+stat_check
 
 echo copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAIED\e[0m"
-    exit 1
-    fi
+  stat_check
 
 echo add Application user
 useradd expense &>>$log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAIED\e[0m"
-    exit 1
-    fi
+ stat_check
 
 echo cleanup content
 rm -rf /app
-
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAIED\e[0m"
-    exit 1
-    fi
+stat_check
 
 mkdir /app
 cd /app
@@ -46,49 +27,24 @@ echo download app content
 
 download_and_extract
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAIED\e[0m"
-    exit 1
-    fi
+stat_check
 
 echo download dependence
 
 npm install &>>$log_file
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAIED\e[0m"
-    exit 1
-    fi
+stat_check
 
 echo restart servoce
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAIED\e[0m"
-    exit 1
-    fi
+stat_check
 
 echo download mysql
 dnf install mysql -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAIED\e[0m"
-    exit 1
-    fi
+stat_check
 echo load schema
 mysql -h 172.31.95.105 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCSEES\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit 1
-    fi
+stat_check
 
--- commen.sh
+++ commen.sh
@@ -10,3 +10,11 @@ echo $?
   unzip /tmp/$component.zip &>>$log_file
   echo $?
 }
+stat_check(){
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCSEES\e[0m"
+    else
+      echo -e "\e[31mFAIED\e[0m"
+      exit 1
+      fi
+}
