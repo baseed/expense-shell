cdab48e4a0de9e72391743b11edd6731ee5d6e13
baseed <baseed08@gmail.com>
2023-10-16 21:49:22 +0530

    expense script

--- mysql.sh
+++ mysql.sh
@@ -18,5 +18,5 @@ stat_check
 
 echo setup root password
 mysql_root_password=$1
-mysql_secure_installation --set-root-pass$mysql_root_password &>>$log_file
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
 stat_check
