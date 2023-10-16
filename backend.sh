ec1e9faadb54888980e7b1b9b290f27e45483976
baseed <baseed08@gmail.com>
2023-10-16 21:30:51 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -52,6 +52,7 @@ echo download mysql client
 dnf install mysql -y &>>$log_file
 stat_check
 echo load schema
-mysql -h mysql.baseed.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+mysql_root_password=$1
+mysql -h mysql.baseed.online -uroot -pmysql_root_password < /app/schema/backend.sql &>>$log_file
 stat_check
 
